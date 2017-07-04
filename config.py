9ff7241b29b2e414847a1dddf17bced7b3c0f1ad
Reck <reckasta@reckasta.gq>
2017-07-04 11:04:59 +1000

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,33 @@
-username = "facility_ai"
-password = "UncloakIsADick"
-client_id = "GORfUXQGjNIveA"
-client_secret = "SzPFXaqgVbRxm_V9-IfGL05npPE"
+# you must register your script in your Reddit account's preferences under 'apps' to get
+# client_id and client_secret, which will authenticate you
+
+
+
+[DEFAULT]
+# A boolean to indicate whether or not to check for package updates.
+check_for_updates=True
+
+# Object to kind mappings
+comment_kind=t1
+message_kind=t4
+redditor_kind=t2
+submission_kind=t3
+subreddit_kind=t5
+
+# The URL prefix for OAuth-related requests.
+oauth_url=https://oauth.reddit.com
+
+# The URL prefix for regular requests.
+reddit_url=https://www.reddit.com
+
+# The URL prefix for short URLs.
+short_url=https://redd.it
+
+[test_bot]
+client_id = GORfUXQGjNIveA
+client_secret = SzPFXaqgVbRxm_V9-IfGL05npPE
+password = UncloakIsADick
+user_agent = Facility_AI Reborne By /u/Reckasta
+
+
+
